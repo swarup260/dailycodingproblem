d8be6650737fb9e86d031c2d76886735b09648c9
swarup <swarup260@gmail.com>
2023-04-19 00:01:43 +0530

    two problems added

---
+++ day1easy.js
@@ -0,0 +1,37 @@
+/**
+ * Given a string of round, curly, and square open and closing brackets, 
+ * return whether the brackets are balanced (well-formed).
+ * For example, given the string "([])[]({})", you should return true.
+ * Given the string "([)]" or "((()", you should return false.
+ */
+
+/**
+ * Using Stack
+ * @param {*} string 
+ * @returns 
+ */
+function isBalancedStack(input){
+    const stack = []
+
+    for (let index = 0; index < input.length; index++) {
+        const char = input[index]
+        if (['[','{','('].includes(char)) stack.push(char)
+        if (['}','}',')'].includes(char)) stack.pop()
+    }
+    return stack.length == 0
+}
+
+/**
+ * Without Using Stack
+ * @param {*} string 
+ * @returns 
+ */
+function isBalanced(input){
+    let isStringBalanced = 0
+    for (let index = 0; index < input.length; index++) {
+        const char = input[index]
+        if (['[','{','('].includes(char)) isStringBalanced ++
+        if (['}','}',')'].includes(char)) isStringBalanced --
+    }
+    return isStringBalanced == 0
+}
---
+++ day2easy.js
@@ -0,0 +1,47 @@
+/**
+ * Run-length encoding is a fast and simple method of encoding strings. 
+ * The basic idea is to represent repeated successive characters as a single count and character. 
+ * For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
+ * Implement run-length encoding and decoding. 
+ * You can assume the string to be encoded have no digits and consists
+ * solely of alphabetic characters. You can assume the string to be decoded is valid.
+ */
+
+function runLengthEncoding(input) {
+    let currentElement = []
+    let encodingString = ''
+
+    for (let index = 0; index <= input.length; index++) {
+        const char = input[index]
+        const charLen = currentElement.length
+        if (charLen > 0 && char != currentElement[charLen - 1]) {
+            encodingString += `${charLen}${currentElement[0]}`
+            currentElement = [] // reset the counter and element
+        }
+        currentElement.push(char)
+    }
+    
+    return encodingString
+}
+
+
+function runLengthDecoding(input) {
+    let decodingString = ''
+
+    for (let index = 0; index < input.length; index += 2) {
+        const charLen = input[index]
+        const char = input[index + 1]
+        for (let index = 0; index < charLen; index++) {
+            decodingString += `${char}`
+        }
+
+    }
+
+    return decodingString
+}
+// EXAMPLE
+// const str = "AAAABBBCCDAAGGGHHHRRTTYQJJUUQPGTY"
+// const encodingString = runLengthEncoding(str)
+// const decodingString = runLengthDecoding(encodingString)
+// console.log({ encodingString, decodingString }, str == decodingString)
+
